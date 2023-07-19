9f68b6aac8d7b5f576228184e903c45d8e5fa5ec
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-07-19 19:27:30 -0300

    feat: base class Entity

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -3,5 +3,6 @@ import { Slug } from './slug'
 
 it('should be able to create a new slug from text', () => {
   const slug = Slug.createFromText('Example question_títle@com.br')
+
   expect(slug.value).toEqual('example-question-titlecombr')
 })
