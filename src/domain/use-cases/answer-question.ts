95112a779a2b6ebbce62faf4f7cb3ce96eb3bc54
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-07-19 00:50:24 -0300

    feat: create entities

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,8 @@
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId }: AnswerQuestionUseCaseRequest) {}
+}
