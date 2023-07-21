429930c1370a4f5c2f7bd490810bfbadae25fd23
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-07-20 22:11:23 -0300

    feat: refactoring folder structure clean arch

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,8 +1,8 @@
 import { AnswerQuestionUseCase } from './answer-question'
 
-import { Answer } from '../entities/answer'
-
 import { AnswersRepository } from '../repositories/answers-repository'
+
+import { Answer } from '../../enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
   create: async (answer: Answer) => {},
