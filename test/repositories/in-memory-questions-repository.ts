e53790061f1d1da4222bd4d7f139304bbcd14583
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-07-26 00:05:26 -0300

    feat: get question by slug test

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,11 +4,17 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
-
-  // async findBySlug(slug: string) {
-  //   const question = this.items.find(item => item.slug.value === slug)
-  // }
 }
