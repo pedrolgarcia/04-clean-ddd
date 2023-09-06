eefc6cb4f6e73d00784032de5abbc00cb2588819
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-09-06 00:23:52 -0300

    feat: on answer created subscriber

---
+++ src/domain/notification/application/subscribers/on-answer-created-spec.ts
@@ -0,0 +1,33 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { OnAnswerCreated } from './on-answer-created'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+
+describe('On Answer Create', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+  })
+  it('should send a notification when an answer is created', () => {
+    const onAnswerCreated = new OnAnswerCreated()
+
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+  })
+})
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -28,13 +29,17 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answers
   }
 
+  async create(answer: Answer) {
+    this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
+  }
+
   async save(answer: Answer) {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
     this.items[itemIndex] = answer
-  }
 
-  async create(answer: Answer) {
-    this.items.push(answer)
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer) {
